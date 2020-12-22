aeb783aca9e103115f0e61b78b2ea4e4d9b3fef6
parkvita <shyeon15@gmail.com>
2020-12-23 00:25:09 +0900

    어노테이션 추가해보기

--- src/main/java/com/parkvita/admin/springboot/web/IndexController.java
+++ src/main/java/com/parkvita/admin/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.parkvita.admin.springboot.web;
 
+import com.parkvita.admin.springboot.config.auth.LoginUser;
 import com.parkvita.admin.springboot.config.auth.dto.SessionUser;
 import com.parkvita.admin.springboot.service.posts.PostsService;
 import com.parkvita.admin.springboot.web.dto.PostsResponseDto;
@@ -27,11 +28,13 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){   // 기존에 (user) httpSession.getAttribute("user")로 가져오던 세션 정보 값이 개선, 이제는 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보 가져올수 있음
         model.addAttribute("posts",postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");  // 앞서 작성된 CustomOAuth2UserService 에서 로그인 성공 시 세션에 SeessionUser를 저장하도록 구성
-                                                                                    // 즉 로그인 성공시 httpSession.getAttribute("user") 에서 값을 가져올 수 있음
+        /* 다른 컨트롤러와 메소드에서도 세션값이 필요하면 반복되기 때문에 메소드 인자로 세션값을 바도록 변경
+       SessionUser user = (SessionUser) httpSession.getAttribute("user");  // 앞서 작성된 CustomOAuth2UserService 에서 로그인 성공 시 세션에 SeessionUser를 저장하도록 구성
+                                                                                    // 즉 로그인 성공시 httpSession.getAttribute("user") 에서 값을 가져올 수 있음*/
+
         if(user!=null){
             // 세션에 저장된 값이 있을때만 model에 userName 으로 등록, 저장된 값이 없으면 model엔 아무런 값이 없는 상태이니 로그인 버튼이 보임
             model.addAttribute("userName",user.getName());
