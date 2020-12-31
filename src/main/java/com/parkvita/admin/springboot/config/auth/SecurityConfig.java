fa22efcdf2c357674b3285935a2e1452542a7bd9
parkvita <shyeon15@gmail.com>
2020-12-31 12:38:22 +0900

    /profile test

--- src/main/java/com/parkvita/admin/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/parkvita/admin/springboot/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션들을 disable 함
                 .and()
                     .authorizeRequests()    // URL별 권한 관리를 설정하는 옵션의 시작점 authorizeRequests가 선언되야만 antMatchers 옵션이 사용가능
-                    .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**").permitAll()
+                    .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**","/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())    // 권한 관리 대상을 지정하는 옵션, URL,HTTP 메소드별로 관리 가능, "/" 등 지정된 URL들은 permitAll() 옵션을 통해 전체 열람권한 "/api/v1/**" 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 함
                     .anyRequest().authenticated()   // 설정된 값들 이외 나머지 URL나타냄, 여기서는 authenticated() 을 추가하여 나머지 URL들은 모두 인증된 사용자들에게만 허용
                 .and()
